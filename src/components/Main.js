28f8e965b4538ad453e81e47b44386de9bff5a48
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-07-31 14:49:45 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {useState} from 'react';
+
+const Main = () => {
+  return (
+    <>
+    <div className='container'>
+       <div className='box'>
+        <h2>Enjoy the Cats</h2>
+        <img src="" />
+        </div> 
+    </div>
+    </>
+  )
+}
+
+export default Main
