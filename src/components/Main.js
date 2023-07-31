599d77a33afbc4600cb1bf4b55218cafdbb1f894
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-07-31 14:51:14 +0200

    add image

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import {useState} from 'react';
+import React from "react";
+import { useState } from "react";
 
 const Main = () => {
   return (
     <>
-    <div className='container'>
-       <div className='box'>
-        <h2>Enjoy the Cats</h2>
-        <img src="" />
-        </div> 
-    </div>
+      <div className="container">
+        <div className="box">
+          <h2>Enjoy the Cats</h2>
+          <img src="https://play-lh.googleusercontent.com/AmKSpZt_rynhOO0ID1eS0gqeW3DFzoH6KNZkAAgepQ0t9MDRQTmil-nlY5GqkZ_7El0" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
